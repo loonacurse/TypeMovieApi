43e996d4e0d04064f625f5d1bb16e0986e3bb032
loonacurse <04darinka10@gmail.com>
2026-06-23 22:31:31 +0300

    Add TelegramBot project

---
+++ TelegramBot/Kursach/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using TelegramBotApi.Model1;
+
+namespace Kursach
+{
+    public class Constants
+    {
+        public static string address = "https://localhost:7108/";
+        public static MongoClient mongoClient;
+        public static IMongoDatabase database;
+        public static IMongoCollection<BsonDocument> collection0;
+        public static IMongoCollection<BsonDocument> movie_list;
+        public static IMongoCollection<BsonDocument> selected_list;
+        public static Selected_Array selected_movie;
+        public static Movie watched_movie = new Movie();
+    }
+}
