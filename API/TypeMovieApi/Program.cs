e62b1348c561429e49b6705ecf55c36f13e47768
loonacurse <04darinka10@gmail.com>
2026-06-23 22:01:06 +0300

    v2 changed DB

--- API/TypeMovieApi/Program.cs
+++ API/TypeMovieApi/Program.cs
@@ -4,7 +4,7 @@ using MongoDB.Driver;
 using System.Text.Json;
 using TelegramBotApi.Model1;
 
-Constants.mongoClient = new MongoClient("mongodb+srv://loonacurse:04(Darinka)10@typemovie.n4kn7xo.mongodb.net/");
+Constants.mongoClient = new MongoClient("mongodb+srv://04darinka10_db_user:2HqltNkjssWIGkbY@typemoviedb.2wbrptk.mongodb.net/");
 Constants.database = Constants.mongoClient.GetDatabase("typemovie");
 Constants.movie_list = Constants.database.GetCollection<BsonDocument>("movie_list");
 Constants.selected_list = Constants.database.GetCollection<BsonDocument>("selected_list");
